8d9f9fbe97d46eb434c141c8800e0be68855edb0
ThomasLeveque <thomasl5895@gmail.com>
2021-07-22 19:46:18 +0200

    Add github domain for avatars

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   images: {
-    domains: ['raw.githubusercontent.com', 'lh3.googleusercontent.com'],
+    domains: [
+      'raw.githubusercontent.com',
+      'avatars.githubusercontent.com',
+      'lh3.googleusercontent.com',
+    ],
   },
 };
