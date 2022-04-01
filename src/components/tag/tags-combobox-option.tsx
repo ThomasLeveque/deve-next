f14269f50e34b3da6cc4a9ffb3a7b5fe8cfee518
ThomasLeveque <thomasl5895@gmail.com>
2022-04-01 22:40:41 +0200

    rework tag combobox with downshift

--- src/components/tag/tags-combobox-option.tsx
+++ src/components/tag/tags-combobox-option.tsx
@@ -1,52 +1,46 @@
 import { useRemoveTag } from '@api/tag/use-remove-tag';
 import SpinnerIcon from '@components/icons/spinner-icon';
-import { Combobox } from '@headlessui/react';
 import { CheckIcon, TrashIcon } from '@heroicons/react/outline';
 import { Tag } from '@models/tag';
 import { useProfile } from '@store/profile.store';
 import classNames from 'classnames';
 import React, { useCallback } from 'react';
 
-interface TagsComboboxOptionProps {
-  selectedTags: Tag[];
+interface TagsComboboxOptionProps extends React.ReactElement<HTMLLIElement> {
   filteredTag: Tag;
-  handleRemoveSelectedTags: (tagId: number) => void;
+  removeSelectedItem: (tag: Tag) => void;
+  active: boolean;
+  selected: boolean;
 }
 
-const TagsComboboxOption: React.FC<TagsComboboxOptionProps> = ({ filteredTag, handleRemoveSelectedTags }) => {
-  const [profile] = useProfile();
-  const removeTag = useRemoveTag();
+const TagsComboboxOption = React.forwardRef<HTMLLIElement, TagsComboboxOptionProps>(
+  ({ active, selected, filteredTag, removeSelectedItem, ...props }, ref) => {
+    const [profile] = useProfile();
+    const removeTag = useRemoveTag();
 
-  const canBeRemove = (filteredTag.links?.length ?? 0) === 0 && profile?.role === 'admin';
+    const canBeRemove = (filteredTag.links?.length ?? 0) === 0 && profile?.role === 'admin';
 
-  const handleRemoveTag = useCallback(
-    (tagId: number, canBeRemove: boolean) => {
-      return async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const handleRemoveTag = useCallback(
+      async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         event.stopPropagation();
         if (canBeRemove) {
-          await removeTag.mutateAsync(tagId);
-          handleRemoveSelectedTags(tagId);
+          await removeTag.mutateAsync(filteredTag.id);
+          removeSelectedItem(filteredTag);
         }
-      };
-    },
-    [handleRemoveSelectedTags, removeTag]
-  );
+      },
+      [canBeRemove, filteredTag, removeSelectedItem, removeTag]
+    );
 
-  return (
-    <Combobox.Option
-      key={filteredTag.id}
-      value={filteredTag}
-      className={({ active }) =>
-        classNames(
+    return (
+      <li
+        className={classNames(
           `pointer grid w-full grid-cols-[20px,1fr] gap-3 px-4 py-2 text-sm hover:bg-primary`,
-          {
-            'bg-primary': active,
-          },
+          { 'bg-primary': active },
           { 'grid-cols-[20px,1fr,20px]': canBeRemove }
-        )
-      }
-    >
-      {({ selected }) => (
+        )}
+        {...props}
+        ref={ref}
+      >
         <>
           <span>{selected && <CheckIcon />}</span>
           <p className="text-left">
@@ -58,20 +52,16 @@ const TagsComboboxOption: React.FC<TagsComboboxOptionProps> = ({ filteredTag, ha
               {removeTag.isLoading ? (
                 <SpinnerIcon />
               ) : (
-                <button
-                  type="button"
-                  onClick={handleRemoveTag(filteredTag.id, canBeRemove)}
-                  className="rounded-[4px] hover:bg-black/10"
-                >
+                <button type="button" onClick={handleRemoveTag} className="rounded-[4px] hover:bg-black/10">
                   <TrashIcon />
                 </button>
               )}
             </>
           )}
         </>
-      )}
-    </Combobox.Option>
-  );
-};
+      </li>
+    );
+  }
+);
 
 export default TagsComboboxOption;
--- src/components/tag/tags-combobox.tsx
+++ src/components/tag/tags-combobox.tsx
@@ -2,10 +2,11 @@ import { useAddTag } from '@api/tag/use-add-tag';
 import { useTags } from '@api/tag/use-tags';
 import SpinnerIcon from '@components/icons/spinner-icon';
 import TagListWrapper from '@components/tag/tag-list-wrapper';
-import { Combobox, Transition } from '@headlessui/react';
+import { Transition } from '@headlessui/react';
 import { InformationCircleIcon, PlusIcon, SelectorIcon } from '@heroicons/react/outline';
 import { Tag as TagModel } from '@models/tag';
 import { formatError } from '@utils/format-string';
+import { useCombobox, useMultipleSelection } from 'downshift';
 import React, { useCallback, useMemo, useState } from 'react';
 import toast from 'react-hot-toast';
 import Tag from '../elements/tag';
@@ -20,34 +21,91 @@ interface TagsComboboxProps {
   errorText?: string;
 }
 
-// WORKs BUT SELECTED INSIDE OPTION DO NOT
 const TagsCombobox: React.FC<TagsComboboxProps> = ({ selectedTags = [], setSelectedTags, className, errorText }) => {
   const [query, setQuery] = useState('');
   const addTag = useAddTag();
 
   const { data: tags } = useTags({ refetchOnMount: false });
 
-  const filteredTags =
-    query === '' ? tags : tags?.filter((tag) => tag.name.toLowerCase().includes(query.toLowerCase()));
-
   const isTagExist = useMemo(
     () => !!tags?.find((tag) => tag.name.toLocaleLowerCase() === query.toLocaleLowerCase()),
     [tags, query]
   );
 
-  const handleChange = useCallback(
-    (tagsToSelect: TagModel[]) => {
-      if (tagsToSelect.length <= MAX_TAGS_LENGTH) {
-        setSelectedTags(tagsToSelect);
-        setQuery('');
-      } else {
-        toast(`${MAX_TAGS_LENGTH} tags max`, {
-          className: 'Info',
-          icon: <InformationCircleIcon />,
-        });
+  const { getSelectedItemProps, getDropdownProps, addSelectedItem, removeSelectedItem, selectedItems } =
+    useMultipleSelection({
+      initialSelectedItems: selectedTags,
+      onSelectedItemsChange: ({ selectedItems }) => {
+        if (selectedItems) {
+          setSelectedTags(selectedItems);
+        }
+      },
+    });
+
+  const filteredTags =
+    query === '' ? tags : tags?.filter((tag) => tag.name.toLowerCase().includes(query.toLowerCase()));
+
+  const {
+    isOpen,
+    getToggleButtonProps,
+    getLabelProps,
+    getMenuProps,
+    getInputProps,
+    getComboboxProps,
+    highlightedIndex,
+    getItemProps,
+  } = useCombobox({
+    inputValue: query,
+    defaultHighlightedIndex: 0,
+    selectedItem: null,
+    items: filteredTags ?? [],
+    stateReducer: (state, actionAndChanges) => {
+      const { changes, type } = actionAndChanges;
+      switch (type) {
+        case useCombobox.stateChangeTypes.InputKeyDownEnter:
+        case useCombobox.stateChangeTypes.ItemClick:
+          return {
+            ...changes,
+            isOpen: true, // keep the menu open after selection.
+          };
+      }
+      return changes;
+    },
+    onStateChange: ({ inputValue, type, selectedItem }) => {
+      switch (type) {
+        case useCombobox.stateChangeTypes.InputChange:
+          setQuery(inputValue ?? '');
+          break;
+        case useCombobox.stateChangeTypes.InputKeyDownEnter:
+        case useCombobox.stateChangeTypes.ItemClick:
+          if (!selectedItem) {
+            break;
+          }
+          if (isSelected(selectedItem.id)) {
+            setQuery('');
+            removeSelectedItem(selectedItem);
+            break;
+          }
+
+          if (selectedItems.length >= MAX_TAGS_LENGTH) {
+            toast('No more than 4 tags', {
+              className: 'Info',
+              icon: <InformationCircleIcon />,
+            });
+            break;
+          }
+          setQuery('');
+          addSelectedItem(selectedItem);
+          break;
+        default:
+          break;
       }
     },
-    [setSelectedTags]
+  });
+
+  const isSelected = useCallback(
+    (itemId: number) => !!selectedItems.find((selectedItem) => selectedItem.id === itemId),
+    [selectedItems]
   );
 
   const handleAddTag = useCallback(async () => {
@@ -56,87 +114,92 @@ const TagsCombobox: React.FC<TagsComboboxProps> = ({ selectedTags = [], setSelec
       const newTag = await addTag.mutateAsync({
         name: tagName,
       });
-      handleChange([...selectedTags, newTag]);
+      addSelectedItem(newTag);
       setQuery('');
     } catch (err) {
       toast.error(formatError(err as Error));
     }
   }, [query, selectedTags]);
 
-  const handleRemoveSelectedTags = useCallback(
-    (tagId: number) => {
-      setSelectedTags(selectedTags.filter((selectedTag) => selectedTag.id !== tagId));
-    },
-    [selectedTags, setSelectedTags]
-  );
-
   return (
-    <Combobox as="div" value={selectedTags} onChange={handleChange} className={className} name="tags">
-      <Combobox.Label className="mb-[6px] ml-1 block font-poppins-bold text-[10px] uppercase text-black">
+    <div className={className}>
+      <label {...getLabelProps()} className="mb-[6px] ml-1 block font-poppins-bold text-[10px] uppercase text-black">
         TAGS (MIN 1, MAX 4)
-      </Combobox.Label>
-      {selectedTags.length > 0 && (
+      </label>
+      {selectedItems.length > 0 && (
         <TagListWrapper className="mb-4">
-          {selectedTags.map((selectedTag) => (
-            <Tag
-              text={selectedTag.name}
-              key={selectedTag.id}
-              isColored
-              isClosable
-              onClose={() => handleRemoveSelectedTags(selectedTag.id)}
-            />
+          {selectedItems.map((selectedItem, index) => (
+            <li key={selectedItem.id} {...getSelectedItemProps({ selectedItem, index })}>
+              <Tag text={selectedItem.name} isColored isClosable onClose={() => removeSelectedItem(selectedItem)} />
+            </li>
           ))}
         </TagListWrapper>
       )}
 
       <div className="relative">
-        <div className="relative z-50 w-full">
-          <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
-            <SelectorIcon className="h-5 w-5 text-gray-600" aria-hidden="true" />
-          </Combobox.Button>
-
-          <Combobox.Input
+        <div {...getComboboxProps()} className="relative  w-full">
+          <input
             placeholder="Search for a tag..."
             className="with-ring h-[50px] w-full rounded-button bg-gray-100 pl-5 pr-10 text-sm placeholder-gray-400"
-            onChange={(event) => setQuery(event.target.value)}
+            {...getInputProps(getDropdownProps({ preventKeyAction: isOpen }))}
           />
+          <button
+            type="button"
+            aria-label="toggle menu"
+            {...getToggleButtonProps()}
+            className="absolute inset-y-0 right-0 flex items-center pr-2"
+          >
+            <SelectorIcon className="h-5 w-5 text-gray-600" aria-hidden="true" />
+          </button>
           {errorText && <p className="absolute top-full right-1 mt-1 text-[10px] text-danger-400">{errorText}</p>}
         </div>
-        <Transition
-          enter="transition duration-100 ease-out"
-          enterFrom="scale-95 opacity-0"
-          enterTo="scale-100 opacity-100"
-          leave="transition duration-75 ease-out"
-          leaveFrom="scale-100 opacity-100"
-          leaveTo="scale-95 opacity-0"
-        >
-          <Combobox.Options className="absolute mt-2 max-h-60 w-full overflow-auto rounded-button bg-gray-100 py-1 shadow-lg focus:outline-none">
-            <li>
-              {!isTagExist && query !== '' && (
-                <button
-                  type="button"
-                  onClick={handleAddTag}
-                  className="grid w-full grid-cols-[20px,1fr] gap-3 px-4 py-2 text-sm hover:bg-primary"
-                >
-                  {addTag.isLoading ? <SpinnerIcon /> : <PlusIcon />}
-                  <p className="text-left">
-                    Create <span className="font-poppins-bold">{query}</span> tag
-                  </p>
-                </button>
-              )}
-            </li>
-            {filteredTags?.map((filteredTag) => (
-              <TagsComboboxOption
-                key={filteredTag.id}
-                filteredTag={filteredTag}
-                selectedTags={selectedTags}
-                handleRemoveSelectedTags={handleRemoveSelectedTags}
-              />
-            ))}
-          </Combobox.Options>
-        </Transition>
+
+        <div {...getMenuProps()}>
+          <Transition
+            show={isOpen}
+            enter="transition duration-100 ease-out"
+            enterFrom="scale-95 opacity-0"
+            enterTo="scale-100 opacity-100"
+            leave="transition duration-75 ease-out"
+            leaveFrom="scale-100 opacity-100"
+            leaveTo="scale-95 opacity-0"
+          >
+            <ul className="absolute z-50 mt-2 max-h-60 w-full overflow-auto rounded-button bg-gray-100 py-1 shadow-lg focus:outline-none">
+              <>
+                <li>
+                  {!isTagExist && query !== '' && (
+                    <button
+                      type="button"
+                      onClick={handleAddTag}
+                      className="grid w-full grid-cols-[20px,1fr] gap-3 px-4 py-2 text-sm hover:bg-primary"
+                    >
+                      {addTag.isLoading ? <SpinnerIcon /> : <PlusIcon />}
+                      <p className="text-left">
+                        Create <span className="font-poppins-bold">{query}</span> tag
+                      </p>
+                    </button>
+                  )}
+                </li>
+                {filteredTags?.map((filteredTag, index) => (
+                  <TagsComboboxOption
+                    key={filteredTag.id}
+                    {...getItemProps({
+                      item: filteredTag,
+                      index,
+                      isSelected: isSelected(filteredTag.id),
+                    })}
+                    filteredTag={filteredTag}
+                    active={highlightedIndex === index}
+                    selected={isSelected(filteredTag.id)}
+                    removeSelectedItem={removeSelectedItem}
+                  />
+                ))}
+              </>
+            </ul>
+          </Transition>
+        </div>
       </div>
-    </Combobox>
+    </div>
   );
 };
 
